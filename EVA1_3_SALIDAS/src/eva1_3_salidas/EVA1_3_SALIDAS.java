681ef17a042431cd80a5718d4b9e59a6eb8c2e72
Jordan-Castaneda <L26550220@chihuahua2.TECNM.MX>
2026-09-03 18:45:30 -0600

    Add files via upload

---
+++ EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_3_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Fundamentos de Programación.");
+        System.out.println("Jordan Fernando Castañeda Aguayo");
+        System.out.print("Tu calificacion es: ");
+        System.out.println(85);
+    }
+    
+}
