d6e4d9be5e99bb67306db356dec92977911b3325
Jordan-Castaneda <L26550220@chihuahua2.TECNM.MX>
2026-09-03 18:45:05 -0600

    Add files via upload

---
+++ EVA1_2_HOLA_MUNDO/src/eva1_2_hola_mundo/EVA1_2_HOLA_MUNDO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_hola_mundo;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_2_HOLA_MUNDO {
+    public static void main(String[] args) {//INICIO DEL PROGRAMA
+        //ESTO ES UN BLOQUE DE CODIGO
+        //MOSTRAR UN MENSAJE DE TEXTO
+        //MOSTRAR --> INSTRUCCION
+        //TEXT --> DATOS
+        System.out.println("Hola mundo!!");
+    }//FIN DEL PROGRAMA
+    
+}
