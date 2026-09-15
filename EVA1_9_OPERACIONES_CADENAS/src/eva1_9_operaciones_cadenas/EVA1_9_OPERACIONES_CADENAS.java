d821ed2eb0dab9a8c67a3cb2c8b51400539fb217
Jordan-Castaneda <L26550220@chihuahua2.TECNM.MX>
2026-09-14 18:55:00 -0600

    Add files via upload

---
+++ EVA1_9_OPERACIONES_CADENAS/src/eva1_9_operaciones_cadenas/EVA1_9_OPERACIONES_CADENAS.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciones_cadenas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_9_OPERACIONES_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TAMBIEN HAY OPERADORES PARA CADENAS
+        //CONCATENACION: PEGAR CADENAS DE TEXTO --> +
+        String cade;
+        cade = "Hola" + "" + "mundo";
+        System.out.println(cade);
+        cade = "Valor = " + 100;
+        System.out.println(cade);
+        cade = "Valores booleanos = " + false;
+        System.out.println(cade);
+    }
+    
+}
