1724501e69e6d83532a86316354fa4d5fbb544b8
Jordan-Castaneda <L26550220@chihuahua2.TECNM.MX>
2026-09-14 18:54:40 -0600

    Add files via upload

---
+++ EVA1_8_OPERACONES_2/src/eva1_8_operacones_2/EVA1_8_OPERACONES_2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operacones_2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_8_OPERACONES_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int resu, val1 =10, val2 = 5;//declarando e INICIALIZANDO
+        //JAVA ES BUENA ONDA: INICIALIZA EN AUTOMATICO LAS VARIABLES:
+        //NUMERO EN CERO, BOOLEANOS EN FALSO
+        //niveles de operadores.
+        resu = 100 + val1 * (val1 / val2);
+        System.out.print("Resu = ");
+        System.out.println(resu);
+    }
+    
+}
